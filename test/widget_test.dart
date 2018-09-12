507150e519ada93cb96f26166d7ee285427ae1ab
Thamizharasu <zarub2k@gmail.com>
2018-09-13 00:22:59 +0530

    Refactoring started

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:focus/main.dart';
+import 'package:focus/src/focus.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new FocusApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
